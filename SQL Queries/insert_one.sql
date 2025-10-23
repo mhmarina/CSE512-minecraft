932e8048eae100cb4a12112ff04317f9adfb3d3a
Azraf Bakri <azraf@bakri.co>
2025-10-23 13:57:42 -0700

    Removed insert queries not needed

--- SQL Queries/insert_one.sql
+++
@@ -1,9 +0,0 @@
-USE minecraft;
-
--- insert one by one
-INSERT INTO servers (ip)
-VALUES (%s)
-ON CONFLICT (ip) DO NOTHING;
-
-INSERT INTO server_data (ip, timestamp, online, latency, num_players, max_players)
-VALUES (%s, %s, %s, %s, %s, %s);
