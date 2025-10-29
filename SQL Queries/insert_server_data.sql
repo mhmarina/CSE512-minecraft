8a5367d9d08a710f9fac342f6ab32b6e6070c142
Azraf Bakri <azraf@bakri.co>
2025-10-29 12:36:23 -0700

    Added the table creation query into the insert query, so everytime query is run it makes sure that the table exist, if not create it

--- SQL Queries/insert_server_data.sql
+++ SQL Queries/insert_server_data.sql
@@ -1,4 +1,16 @@
 USE minecraft;
 
+-- Create Table if does not exist
+CREATE TABLE IF NOT EXISTS server_data (
+    ip STRING NOT NULL REFERENCES servers(ip) ON DELETE CASCADE,
+    timestamp TIMESTAMP NOT NULL,
+    num_players INT,
+    online BOOL,
+    latency FLOAT,
+    max_players INT,
+    PRIMARY KEY (ip, timestamp DESC)
+);
+
+--Insert data to server_data table
 INSERT INTO server_data (ip, timestamp, online, latency, num_players, max_players)
 VALUES %s;
--- SQL Queries/insert_servers.sql
+++ SQL Queries/insert_servers.sql
@@ -1,5 +1,11 @@
 USE minecraft;
 
+-- Create Table if does not exist
+CREATE TABLE IF NOT EXISTS servers (
+    ip STRING PRIMARY KEY
+);
+
+--Insert data to servers table
 INSERT INTO servers (ip)
 VALUES %s
 ON CONFLICT (ip) DO NOTHING;
