d5c2e98a909663a935bf2dc3c735bf12af3d2622
Marina <140115877+mhmarina@users.noreply.github.com>
2025-11-18 18:36:52 -0700

    Assets etc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,36 +5,39 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
   return (
-    <div style={{justifyContent: "center", display: "flex", flexDirection: "column"}}>
-      <h1>Minecraft Data Visualizer</h1>
+    <div style={{justifyContent: "center", display: "flex", flexDirection: "column", padding: "50px"}}>
+      <h1 style={{fontSize: "4.7em"}}>Minecraft Data Visualizer</h1>
       <div style={{marginBottom: "100px"}}></div>
-      <h2>Top Servers by Uptime</h2>
-      <RankingsContainer 
-        metric="uptime"
-        numRankings={150}
-      />
-      <h2>Top Servers by Capacity</h2>
-      <RankingsContainer 
-        metric="capacity"
-        numRankings={150}
-      />
-      <div style={{marginBottom: "100px"}}></div>
-      <h2 style={{marginBottom: "50px"}}>Lookup your Server by IP Address</h2>
-      <SearchContainer/>
-      <div style={{marginBottom: "100px"}}></div>
-      <div style={{textAlign: 'left', display: "flex", alignSelf: "center", justifySelf: "center"}}>
-        <h2>Definitions</h2>
-        <ul>
-          <li>Capacity: number of active users / number of maximum users</li>
-          <li>Uptime: average time online</li>
-        </ul>
-      </div>
-      <a style={{alignSelf: "flex-end"}} target="_blank" href="https://github.com/mhmarina/CSE512-minecraft">       
-        <FontAwesomeIcon 
-          icon={faGithub}
-          style={{fontSize: "25px"}}
+
+      <div className="sectionContainer">
+        <h2>Top Servers by Uptime</h2>
+        <RankingsContainer 
+          metric="uptime"
+          numRankings={150}
         />
-      </a>
+        <h2>Top Servers by Capacity</h2>
+        <RankingsContainer 
+          metric="capacity"
+          numRankings={150}
+        />
+      </div>
+
+      <div className="sectionContainer">
+        <h2>Lookup your Server by IP Address</h2>
+        <SearchContainer/>
+      </div>
+
+      <div>
+        <h2>Definitions</h2>
+        <h3>Capacity: number of active users / number of maximum users</h3>
+        <h3>Uptime: average time online</h3>
+        <a style={{alignSelf: "flex-end"}} target="_blank" href="https://github.com/mhmarina/CSE512-minecraft">       
+          <FontAwesomeIcon 
+            icon={faGithub}
+            style={{fontSize: "25px"}}
+          />
+        </a>
+      </div>
     </div>
   )
 }
